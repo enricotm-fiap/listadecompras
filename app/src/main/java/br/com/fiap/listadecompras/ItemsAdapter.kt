e638f0355e55a92aca62621043094d6c526f01f7
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-28 20:28:31 -0300

    Implementando rotina de exclusão (aula2808)

--- app/src/main/java/br/com/fiap/listadecompras/ItemsAdapter.kt
+++ app/src/main/java/br/com/fiap/listadecompras/ItemsAdapter.kt
@@ -1,23 +1,49 @@
 package br.com.fiap.listadecompras
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class ItemsAdapter : RecyclerView.Adapter<ItemsAdapter.ItemViewHolder>() {
 
+    private val items = mutableListOf<ItemModel>()
+
     class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        val textView = view.findViewById<TextView>(R.id.textViewItem)
+        val button = view.findViewById<ImageButton>(R.id.imageButton)
+
+        fun bind(item: ItemModel) {
+            textView.text = item.name
+
+            button.setOnClickListener {
+                item.onRemove(item)
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_layout, parent, false)
+        return ItemViewHolder(view)
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = items.size
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val item = items[position]
+        holder.bind(item)
+    }
+
+    fun addItem(newItem: ItemModel) {
+        items.add(newItem)
+        notifyDataSetChanged()
+    }
+
+    fun removeItem(item: ItemModel) {
+        items.remove(item)
+        notifyDataSetChanged()
     }
 }
